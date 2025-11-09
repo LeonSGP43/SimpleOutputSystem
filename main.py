a542a7c1592554e941a238d86ec2e0f05bb98319
LeonCN <154585401+LeonSGP43@users.noreply.github.com>
2025-11-09 20:23:21 +0800

    feat:new fucnitons (#1)

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=app.config["PORT"], debug=app.config["DEBUG"])
+    print("Hello World")
+    print(app.config)
+    print(app.config["PORT"])
+    print(app.config["DEBUG"])
